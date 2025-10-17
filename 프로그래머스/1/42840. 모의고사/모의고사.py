436a1364283dae8f43783b17b296370c801fa42d
Mr.Song <lssyl_@naver.com>
2025-10-17 11:49:27 +0900

    [level 1] Title: 모의고사, Time: 6.34 ms, Memory: 9.31 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,21 @@
+def solution(answers):
+    first = [1,2,3,4,5]
+    second = [2,1,2,3,2,4,2,5]
+    third = [3,3,1,1,2,2,4,4,5,5]
+    
+    score = [0,0,0]
+    result = []
+    for i in range(len(answers)):
+        if answers[i] == first[i % len(first)]:
+            score[0] += 1
+        if answers[i] == second[i % len(second)]:
+            score[1] += 1
+        if answers[i] == third[i % len(third)]:
+            score[2] += 1
+    
+    max_score = max(score)
+    
+    for i in range(len(score)):
+        if score[i] == max_score:
+            result.append(i+1)
+    return result
