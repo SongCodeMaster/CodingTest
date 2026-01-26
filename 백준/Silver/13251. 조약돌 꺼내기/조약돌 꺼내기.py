2b47d7c0682b1af68c04b151a4aecb932985f34a
Mr.Song <lssyl_@naver.com>
2026-01-26 22:45:21 +0900

    [Silver III] Title: 조약돌 꺼내기, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.py
+++ 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.py
@@ -1,39 +1,16 @@
-# 조약돌꺼내기 문제 10시30분까지
-# M은 색상 N은 조약돌 K는 뽑는개수
-# 각 색상의 조약돌이 몇개있는지
-
-# 전체 뽑는 경우의수 / 같은 색 뽑을 확률들 더하기
-# 18C2 / 5C2 + 6C2 + 7C2
-
-# 5C2 = 5*4 // 2!
-def combination(n,k):
-    if n < k:
-        return 0
-    
-    result = 1
-
-    for i in range(n,n-k,-1):
-        result *= i
-    
-    for j in range(1,k+1):
-        result = result // j
-
-    return result
+import math
 
 M = int(input())
 N = list(map(int, input().split()))
 K = int(input())
 
-# 18C2 / 5C2 + 6C2 + 7C2
-# 확률계산하기
+total_pebbles = sum(N)
+# 분모: 전체에서 K개 뽑기
+bottom = math.comb(total_pebbles, K)
 
-# 확률 계산
-bottom = combination(sum(N),K)
-
-# top 경우의수
+# 분자: 각 색깔별로 K개 뽑는 경우의 수 더하기
 top = 0
-for i in range(len(N)):
-    n = N[i]
-    top += combination(n,K)
+for n in N:
+    top += math.comb(n, K)
 
 print(top / bottom)
