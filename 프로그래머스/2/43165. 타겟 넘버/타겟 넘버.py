542d786cfeb7153c43346db3e0de8961ba148262
Mr.Song <lssyl_@naver.com>
2026-04-06 16:21:09 +0900

    [level 2] Title: 타겟 넘버, Time: 384.19 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,17 +1,23 @@
-count = 0
-
-def dfs(numbers,target,idx,value):
+def dfs(numbers,target,sum,idx):
     global count
-    if len(numbers) == idx:
-        if value == target:
+    
+    if idx == len(numbers):
+        if target == sum:
             count += 1
         return
             
-    dfs(numbers,target,idx+1, value + numbers[idx])
-    dfs(numbers,target,idx+1, value - numbers[idx])
+    
+    
+    # 빼기 dfs
+    dfs(numbers, target, sum + numbers[idx], idx+1)
+    # 더하기 dfs
+    dfs(numbers, target, sum - numbers[idx], idx+1)
+    
     
 def solution(numbers, target):
     global count
-    count = 0
-    dfs(numbers,target,0,0)
+    count = 0 
+    sum = 0
+    
+    dfs(numbers, target, sum, 0)
     return count
