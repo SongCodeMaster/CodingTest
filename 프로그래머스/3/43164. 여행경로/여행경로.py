2383648becaf4e7b6d68cf756177deeb3951c490
Mr.Song <lssyl_@naver.com>
2026-04-22 15:47:47 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,27 +1,23 @@
 def solution(tickets):
     tickets.sort(reverse=True)
-    print(tickets)
+    
     graph = {}
-    
-    for start, end in tickets:
-        if start not in graph:
-            graph[start] = []
-        graph[start].append(end)
-    
     stack = ["ICN"]
     path = []
     
+    for a,b in tickets:
+        if a not in graph:
+            graph[a] = []
+        graph[a].append(b)
+        
     while stack:
         top = stack[-1]
         
         if top in graph and len(graph[top]) > 0:
-            next_airplain = graph[top].pop()
-            stack.append(next_airplain)
-
+            next_travle = graph[top].pop()
+            stack.append(next_travle)
         else:
             path.append(stack.pop())
-    
-    return path[::-1]
             
-    answer = []
-    return answer
+    return path[::-1]
+        
