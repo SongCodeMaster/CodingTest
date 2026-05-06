d8dca86fc9f66247270a0716e09a2070442deb68
Mr.Song <lssyl_@naver.com>
2026-05-06 22:36:33 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 4.39 ms, Memory: 14.6 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -1,7 +1,10 @@
 def solution(arr):
-    if(len(arr) == 1):
+    if len(arr) == 1:
         return [-1]
     
-    arr.remove(min(arr))
-        
+    min_value = arr[0]
+    for i in range(1,len(arr)):
+        if arr[i] < min_value:
+            min_value = arr[i]
+    arr.remove(min_value)
     return arr
