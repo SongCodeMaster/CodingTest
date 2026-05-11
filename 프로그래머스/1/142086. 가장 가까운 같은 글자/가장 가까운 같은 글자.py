9c021a6bdfcb4ce2b73c41e73055344333a75058
Mr.Song <lssyl_@naver.com>
2026-05-11 21:20:55 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 0.89 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,17 +1,11 @@
 def solution(s):
     answer = []
-    # 각 문자의 '마지막 위치'를 저장할 딕셔너리
-    last_pos = {}
-    
-    # enumerate를 쓰면 인덱스와 글자를 동시에 가져온다
-    for i, char in enumerate(s):
-        if char in last_pos:
-            # 현재위치 - 마지막 위치
-            answer.append(i - last_pos[char])
+    word_dict = {}
+    for i, word in enumerate(s):
+        if word in word_dict:
+            answer.append(i - word_dict[word])
+            word_dict[word] = i
         else:
+            word_dict[word] = i
             answer.append(-1)
-        
-        # 현재 글자의 위치를 딕셔너리에 업데이트
-        last_pos[char] = i
-        
     return answer
