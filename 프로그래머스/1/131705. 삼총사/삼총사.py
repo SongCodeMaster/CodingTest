5e3aee3fce760f76288f92b2ac8e59e82db177af
Mr.Song <lssyl_@naver.com>
2026-05-11 17:49:00 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,17 +1,8 @@
+from itertools import combinations
+
 def solution(number):
     answer = 0
-    result = 0
-    for i in range(0, len(number)):
-        for j in range(1, len(number)):
-            if i >= j:
-                continue
-            for k in range(2, len(number)):
-                if j >= k or i >= k:
-                    continue
-                answer = number[i] + number[j] + number[k]
-                if answer == 0:
-                    result += 1
-                else:
-                    answer = 0
-                
-    return result
+    for i in combinations(number,3):
+        if sum(i) == 0:
+            answer += 1
+    return answer
