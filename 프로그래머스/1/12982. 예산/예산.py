e21d4b609d6409565e5f5a37cda3cfed615c1e30
Mr.Song <lssyl_@naver.com>
2026-05-18 22:30:58 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,17 +1,14 @@
 def solution(d, budget):
-    d.sort()
-    count = 0
-    
-    if sum(d) <= budget:
+    if sum(d) < budget:
         return len(d)
+        
+    count = 0
+    d.sort()
 
-        
-    for i in range(len(d)):
+    for i in d:
+        if budget < i:
+            break
+        budget -= i
         count += 1
-        budget -= d[i]
-        if budget == 0:
-            return count
-        elif budget < 0:
-            return count - 1
         
-        
+    return count
