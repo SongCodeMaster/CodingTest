d7e12024b64391ecdbb0e7e97c41a994ea0d0c2d
Mr.Song <lssyl_@naver.com>
2026-05-18 22:30:45 +0900

    [level 3] Title: 단어 변환, Time: 0.39 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,31 +1,32 @@
 from collections import deque
-def one_diff(word1, word2):
+def diff_word(word1, word2):
     count = 0
     for i in range(len(word1)):
         if word1[i] != word2[i]:
-            count +=1
-
+            count += 1
+    
     if count == 1:
         return True
     else:
         return False
     
 def solution(begin, target, words):
-    if target not in words:
-        return 0
-    
-    
-    visited = {i:0 for i in words}
+    answer = 0
+    visited = { i: 0 for i in words }
     
     q = deque()
     q.append((begin, 0))
+    visited[begin] = 1
     
     while q:
-        current_word, current_dist = q.popleft()
-        if current_word == target:
-            return current_dist
-        for next_word in words:
-            if one_diff(current_word, next_word) and visited[next_word] == 0:
-                visited[next_word] = 1
-                q.append((next_word, current_dist+1))
+        current_word, dist = q.popleft()
         
+        if current_word == target:
+            return dist
+        
+        for next_word in words:
+            if visited[next_word] == 0 and diff_word(current_word, next_word):
+                visited[next_word] = 1
+                q.append((next_word, dist + 1))
+        
+    return 0
