041b2e431c3d3ea56a5cfe6f862bb57fa6fcdbaf
Mr.Song <lssyl_@naver.com>
2026-05-18 22:30:35 +0900

    [level 2] Title: 주식가격, Time: 17.25 ms, Memory: 18.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,16 +1,17 @@
 def solution(prices):
-    answer = [0] * (len(prices))
+    # (주식값, 인덱스)
     stack = []
+    result = [0] * (len(prices))
     
     for i in range(len(prices)):
         while stack and stack[-1][0] > prices[i]:
             value, index = stack.pop()
-            answer[index] = i-index
+            result[index] = i - index
         
         stack.append((prices[i], i))
-        
+    
     while stack:
         value, index = stack.pop()
-        answer[index] = len(prices) - 1 - index 
+        result[index] = len(prices) - 1 - index
         
-    return answer
+    return result
