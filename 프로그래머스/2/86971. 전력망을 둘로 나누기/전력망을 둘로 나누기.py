b18ecdf6371a295ea5cdaaced30ec3539703659f
Mr.Song <lssyl_@naver.com>
2026-05-25 22:27:54 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.06 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,27 +1,30 @@
-# 후위순회하며 서브트리 값 최솟값 갱신 비교
 def solution(n, wires):
     answer = 101
-    visited = [False] * (n+1)
-    tree = [[] for _ in range(n+1)]
     
-    for u,v in wires:
+    tree = [[] for _ in range(n+1)]
+    visited = [False] * (n+1)
+    
+    for u, v in wires:
         tree[u].append(v)
         tree[v].append(u)
-    
+        
     def dfs(node):
         nonlocal answer
         visited[node] = True
+        
         my_tree_size = 1
+        
         for next in tree[node]:
             if not visited[next]:
                 child_size = dfs(next)
-                diff = abs((child_size) - (n - child_size))
                 
-                answer = min(diff, answer)
+                diff = abs((child_size) - (n - child_size))
+                answer = min(answer, diff)
                 
                 my_tree_size += child_size
                 
         return my_tree_size
-    
+        
     dfs(1)
+    
     return answer
