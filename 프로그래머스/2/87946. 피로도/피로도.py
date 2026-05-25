643203ab12fdd2e752059a5d77c644c952d65c23
Mr.Song <lssyl_@naver.com>
2026-05-25 22:27:47 +0900

    [level 2] Title: 피로도, Time: 16.84 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,17 +1,17 @@
 from itertools import permutations
 
 def solution(k, dungeons):
-    max_dungeons = 0
+    max_count = 0
     for order in permutations(dungeons, len(dungeons)):
-        current_k = k
         count = 0
-        for dungeon_k, use_k in order:
-            if current_k >= dungeon_k:
+        current_k = k
+        for want_k, use_k in order:
+            if current_k >= want_k:
                 current_k -= use_k
                 count += 1
             else:
                 break
-            
-        max_dungeons = max(max_dungeons, count)
-            
-    return max_dungeons
+        
+        max_count = max(max_count, count)
+        
+    return max_count
