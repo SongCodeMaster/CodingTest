b4e1347ea62ba03fa8a656ccc99d31bd2ead6780
Mr.Song <lssyl_@naver.com>
2026-06-11 13:48:54 +0900

    [level 2] Title: 올바른 괄호, Time: 5.20 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,13 +1,13 @@
 def solution(s):
-    if s[0] == ')':
+    if s[0] == ")":
         return False
     
     stack = []
     
     for i in s:
-        if i == '(':
-            stack.append('(')
-        elif i == ')' and len(stack) > 0:
+        if i == "(":
+            stack.append(i)
+        elif i == ")" and len(stack) > 0:
             stack.pop()
     
     if len(stack) == 0:
