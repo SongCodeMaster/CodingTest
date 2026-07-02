1f373e3382509828a6e1d3eea4d9714b7889426c
Mr.Song <lssyl_@naver.com>
2026-07-02 17:45:56 +0900

    [level 1] Title: 폰켓몬, Time: 1.47 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,2 +1,17 @@
 def solution(nums):
-    return min(len(set(nums)), len(nums) // 2)
+    nums.sort()  
+    
+    unique_count = 1 
+    max_pick = len(nums) // 2
+    
+    if unique_count == max_pick:
+        return max_pick
+    
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i-1]:
+            unique_count += 1
+            
+            if unique_count == max_pick:
+                return max_pick
+            
+    return unique_count
