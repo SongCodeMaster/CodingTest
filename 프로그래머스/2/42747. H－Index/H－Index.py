4aa25a5b3e2d7ac69598b7113c53fb3f2f893b21
Mr.Song <lssyl_@naver.com>
2026-07-12 21:45:10 +0900

    [level 2] Title: H-Index, Time: 0.11 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,9 @@
+# 6,5,3,1,0
+def solution(citations):
+    citations.sort(reverse=True)
+    
+    for i in range(len(citations)):
+        if citations[i] < i + 1:
+            return i
+    
+    return len(citations)
